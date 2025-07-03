ba609559d9a67ce87bdfb364b9052762f661822d
Tấn Tài <tantai13102005@gmail.com>
2025-07-03 04:44:05 +0000

    fix: handle missing vehicle in UCSSolver

--- src/solver.py
+++ src/solver.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from board import Board
 
-#import library for UCS
 import time
 import heapq
 import resource
@@ -35,9 +34,11 @@ class Solver(ABC):
         }
 class UCSSolver(Solver):
     def solve(self):
+        self.nodes_expanded = 0
+        self.solution = None
+        self.memory_usage = 0
         start_time = time.time()
         initial_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-
         # Priority queue: (cost, counter, path, board)
         # A counter is used to break ties in the priority queue
         counter = 0
@@ -64,21 +65,26 @@ class UCSSolver(Solver):
                 board_repr = repr(new_board)
 
                 if board_repr not in visited:
-                    visited.add(board_repr)
-                    
-                    # Find the vehicle that moved to get its length for the cost
                     moved_vehicle = None
                     for vehicle in current_board.vehicles:
                         if vehicle.id == move.vehicle_id:
                             moved_vehicle = vehicle
                             break
+
+                    if moved_vehicle is None:
+                        raise ValueError(f"No vehicle found with id {move.vehicle_id} in current_board.vehicles")
                     
                     new_cost = cost + moved_vehicle.length
                     new_path = path + [move]
                     counter += 1
                     heapq.heappush(frontier, (new_cost, counter, new_path, new_board))
-        
+                    new_path = path + [move]
         # If no solution is found
+        self.search_time = time.time() - start_time
+        final_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        self.memory_usage = (final_memory - initial_memory) / 1024  # in KB
+        self.solution = None
+        return None
         self.search_time = time.time() - start_time
         self.solution = None
         return None
