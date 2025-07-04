8f029285600d8566226c9955afb06da37f6ab0c7
pnthai285 <pnthai0000@gmail.com>
2025-07-04 02:57:52 +0000

    add astar algorithm with cross-platform memory count

--- src/solver/algorithms/astar.py
+++ src/solver/algorithms/astar.py
@@ -1,11 +1,11 @@
 import time
 import heapq
-import resource
+import psutil  
+import os      
 
 from ..base import Solver
 from board import Board
 from move import Move
-
 
 class AStarSolver(Solver):
     """
@@ -29,7 +29,6 @@ class AStarSolver(Solver):
             if cell_content != '.':
                 blocking_vehicle_ids.add(cell_content)
         
-        # Sum the lengths of the unique blocking vehicles.
         h_cost = sum(vehicle_map[vid] for vid in blocking_vehicle_ids)
         return h_cost
 
@@ -37,14 +36,16 @@ class AStarSolver(Solver):
         self.nodes_expanded = 0
         self.solution = None
         self.memory_usage = 0
+        
+        process = psutil.Process(os.getpid())  # <-- CHANGE
         start_time = time.time()
-        initial_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        initial_memory = process.memory_info().rss  # <-- CHANGE
+        peak_memory = initial_memory  # <-- ADD
 
         initial_board = self.board
         initial_board_repr = repr(initial_board)
         vehicle_map = self._get_vehicle_map(initial_board)
 
-        # Priority queue: (f_cost, g_cost, counter, board)
         counter = 0
         h_cost = self._heuristic(initial_board, vehicle_map)
         frontier = [(h_cost, 0, counter, initial_board)]
@@ -54,6 +55,12 @@ class AStarSolver(Solver):
         g_cost_so_far = {initial_board_repr: 0}
 
         while frontier:
+            # --- Peak Memory Tracking inside the loop ---
+            current_memory = process.memory_info().rss # <-- ADD
+            if current_memory > peak_memory:            # <-- ADD
+                peak_memory = current_memory            # <-- ADD
+            # --------------------------------------------
+
             _, g_cost, _, current_board = heapq.heappop(frontier)
             current_board_repr = repr(current_board)
 
@@ -64,16 +71,11 @@ class AStarSolver(Solver):
 
             if current_board.is_solved():
                 self.solution = self._path_construct(came_from, current_board_repr)
-                self.search_time = time.time() - start_time
-                final_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                self.memory_usage = (final_memory - initial_memory) / 1024  
-                return self.solution
+                break # <-- CHANGE: Break the loop to do final calculations outside
 
-            # We need a map for the current board's vehicles for quick length lookups.
             current_vehicle_map = self._get_vehicle_map(current_board)
 
             for move in current_board.get_possible_moves():
-                # The cost of this move is the length of the vehicle being moved.
                 move_cost = current_vehicle_map[move.vehicle_id]
                 new_g_cost = g_cost + move_cost
                 
@@ -84,19 +86,19 @@ class AStarSolver(Solver):
                     g_cost_so_far[new_board_repr] = new_g_cost
                     came_from[new_board_repr] = (current_board_repr, move)
                     
-                    # Calculate the new f_cost using the weighted g_cost and new heuristic.
                     h_cost = self._heuristic(new_board, vehicle_map)
                     f_cost = new_g_cost + h_cost
                     
                     counter += 1
                     heapq.heappush(frontier, (f_cost, new_g_cost, counter, new_board))
 
-        # If no solution is found
         self.search_time = time.time() - start_time
-        final_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        self.memory_usage = (final_memory - initial_memory) / 1024  # in KB
-        self.solution = None
-        return None
+        final_memory = process.memory_info().rss # <-- CHANGE
+        if final_memory > peak_memory:          # <-- ADD
+            peak_memory = final_memory          # <-- ADD
+        self.memory_usage = (peak_memory - initial_memory) / 1024  
+        
+        return self.solution
 
     def _path_construct(self, came_from: dict, current_board_repr: str):
         path = []
@@ -105,5 +107,4 @@ class AStarSolver(Solver):
             if move:
                 path.append(move)
             current_board_repr = parent_repr
-        # The path is constructed backward, so we reverse it to get start -> end.
         return path[::-1]
